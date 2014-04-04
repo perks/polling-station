da83ba6bce7b8e2558c1ca0baaec3687c9954699
Chris Evans <chris.evans93@gmail.com>
2014-04-04 14:07:21 -0400

    Event binding done; recalibrate data on click

--- polling.js
+++ polling.js
@@ -1,40 +1,64 @@
- function PollingWidget(poll) {
+ function PollingWidget(poll, template_func) {
 
      var that = this;
 
      if (typeof poll == 'string' || poll instanceof String) {
          loadPoll(poll, function(xhr) {
-             this.poll = JSON.parse(xhr.responseText);
+             that.poll = JSON.parse(xhr.responseText);
+             var el = that.renderChoices(template_func);
+             that.bindPollToChoices(el);
          });
-     } else if {
-         typeof poll == 'object' || poll instanceof Object) {
+     } else if (typeof poll == 'object' || poll instanceof Object) {
          this.poll = poll;
+         this.bindPollToChoices(el);
      } else {
          console.log("Poll argument not recognized; functionality may break");
          this.poll = poll;
+         this.bindPollToChoices(el);
      }
-
  }
 
- PollingWidget.prototype.bindPoll = function(el) {
-     var choices = el.querySelectAll('div.poll-result');
-     var forEach = Array.prototype.forEach;
+ PollingWidget.prototype.renderChoices = function(template_func) {
+     var html = template_func(this.poll);
+     document.getElementById('polling-widget').innerHTML = html;
+     return document.getElementById('poll-choices');
+ }
 
-     if (choices) {
-         forEach.call(choices, function(el) {
+ PollingWidget.prototype.bindPollToChoices = function(el) {
+     this.choices = el.querySelectorAll('.poll-choice');
+     var forEach = Array.prototype.forEach;
+     var that = this;
+
+     if (this.choices) {
+         forEach.call(that.choices, function(el) {
              var selection = el.getAttribute('data-id');
              that.choices[selection] = el;
              el.addEventListener('click', function(e) {
                  e.preventDefault();
-                 console.log('I got clicked on id: ' + selection);
+                 that.recalibratePoll();
              });
          });
      }
-
  };
 
+PollingWidget.prototype.recalibratePoll = function() {
+    this.poll.total = 0;
+    var choice_arr = this.poll.choices;
+    for(var i in choice_arr) {
+        this.poll.total += choice_arr[i].count;
+
+    }
+
+    console.log(this.poll.total);
+
+    for(var j in choice_arr) {
+        var percent = Math.round((this.poll.choices[j].count/this.poll.total)*100);
+        this.poll.choices[j].percent = percent
+    }
+};
 
  function loadPoll(url, callback) {
+     console.log('loading');
 
      xhr = new XMLHttpRequest();
 
@@ -50,6 +74,7 @@
          }
 
          if (xhr.readyState == 4) {
+             console.log('calling back');
              callback(xhr);
          }
      }
@@ -74,6 +99,5 @@
      "total": 0
  };
 
- var template = Handlebars.compile(document.getElementById('choice-template').innerHTML());
- var myPoll = new PollingWidget('data.json');
- var el = template(myPoll.poll);
+ var template = Handlebars.compile(document.getElementById('choice-template').innerHTML);
+ var myPoll = new PollingWidget('data.json', template);
